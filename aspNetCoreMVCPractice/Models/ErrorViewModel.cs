a343eca2f8e63273474e6f05b4a77170d00bd85a
Aiden Miller <bonsoirelliot@gmail.com>
2022-02-04 22:02:12 -0500

    Add project files.

---
+++ aspNetCoreMVCPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspNetCoreMVCPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
